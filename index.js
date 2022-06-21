da53f1d2a3a75439f7d15baff5c7318ba3692caf
Genrry Uwualdo Luis Alvarado <88805705+gluis2020232@users.noreply.github.com>
2022-06-21 01:48:39 -0600

    Configuración Heroku

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const app = require('./app');
 mongoose.Promise = global.Promise;
 
 //mongoose.connect('mongodb://localhost:27017/Torneo_Deportivo', { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
-
+//Conexión con mongodb atlas
 mongoose.connect('mongodb+srv://gluis:gluis123@cluster0.obp9ldu.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
 
     console.log('Se ha conectado correctamente a la base de datos.');
 
-    app.listen(1500, function() {
+    app.listen(process.env.PORT || 1500, function() {
         console.log("Servidor de Express corriendo correctamente en el puerto 1500");
     });
 
