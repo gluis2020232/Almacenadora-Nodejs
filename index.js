991422207a9d2cc35e0664adfeee0493bac4beac
Genrry Uwualdo Luis Alvarado <88805705+gluis2020232@users.noreply.github.com>
2022-06-21 00:42:33 -0600

    Conexión con MongoDB Atlas

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
 const app = require('./app');
 
-
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/Torneo_Deportivo', { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+
+//mongoose.connect('mongodb://localhost:27017/Torneo_Deportivo', { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+
+mongoose.connect('mongodb+srv://gluis:gluis123@cluster0.obp9ldu.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+
     console.log('Se ha conectado correctamente a la base de datos.');
 
     app.listen(1500, function() {
